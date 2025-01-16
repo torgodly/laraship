f1c85f46f3c05d9353996649eb234a78bf6693b9
Abdullah Alhaj <torgodly@gmail.com>
2025-01-17 00:11:45 +0200

    WIP

--- resources/views/Scripts/DatabaseScripts/link_user_to_database.blade.php
+++ resources/views/Scripts/DatabaseScripts/link_user_to_database.blade.php
@@ -2,7 +2,7 @@
 # Loop through each database and grant the user access
 @foreach($databases as $database)
     echo "Linking user '{{$username}}' to database '{{$database}}'..."
-    mysql -e "GRANT ALL PRIVILEGES ON {{$database}}.* TO '{{$username}}'@'localhost';"
+    mysql -e "GRANT ALL PRIVILEGES ON {{$database}}.* TO '{{$username}}'@'164.92.167.192';"
     mysql -e "FLUSH PRIVILEGES;"
     echo "User '{{$username}}' has been linked to '{{$database}}'."
 @endforeach
