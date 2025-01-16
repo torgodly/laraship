b5a4bdbd023561e0c3c5ecdbc15974079bad407c
Abdullah Alhaj <torgodly@gmail.com>
2025-01-17 01:19:58 +0200

    WIP

--- resources/views/Scripts/DatabaseScripts/create_database_user.blade.php
+++ resources/views/Scripts/DatabaseScripts/create_database_user.blade.php
@@ -1,5 +1,6 @@
 
 # Create the user and grant privileges
 mysql -u root -e "CREATE USER IF NOT EXISTS '{{$username}}'@'localhost' IDENTIFIED BY '{{$password}}';"
+mysql -u root -e "CREATE USER IF NOT EXISTS '{{$username}}'@'%' IDENTIFIED BY '{{$password}}';"
 echo "User '{{$username}}' has been created."
 
--- resources/views/Scripts/DatabaseScripts/link_user_to_database.blade.php
+++ resources/views/Scripts/DatabaseScripts/link_user_to_database.blade.php
@@ -2,6 +2,7 @@
 # Loop through each database and grant the user access
 @foreach($databases as $database)
     mysql -e "GRANT ALL PRIVILEGES ON {{$database}}.* TO '{{$username}}'@'164.92.167.192';"
+    mysql -e "GRANT ALL PRIVILEGES ON {{$database}}.* TO '{{$username}}'@'%';"
     mysql -e "FLUSH PRIVILEGES;"
     echo "User '{{$username}}' has been linked to '{{$database}}'."
 @endforeach
