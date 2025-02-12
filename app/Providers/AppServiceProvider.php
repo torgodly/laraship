a461467c5c50d95aa4825f0ae1444b53b17a34d5
Abdullah Alhaj <torgodly@gmail.com>
2025-02-12 18:52:55 +0200

    feat: force HTTPS scheme in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //force over https
+        \URL::forceScheme('https');
     }
 }
