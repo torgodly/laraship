a738960881845ad3d0d5c9514cba42934cd18027
Abdullah Alhaj <torgodly@gmail.com>
2025-03-16 04:11:51 +0200

    fix: update EditAction reference in SiteResource for correct namespace usage

--- app/Filament/Resources/SiteResource.php
+++ app/Filament/Resources/SiteResource.php
@@ -55,7 +55,7 @@ class SiteResource extends Resource
                 ],
             )
             ->actions([
-                EditAction::make()
+                Tables\Actions\EditAction::make()
             ]);
     }
 
