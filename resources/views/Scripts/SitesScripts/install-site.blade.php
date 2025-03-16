95d4b2eb1ea674ec51005f000c1a5a9ed989028f
Abdullah Alhaj <torgodly@gmail.com>
2025-03-16 04:57:38 +0200

    fix: update database variable handling in install-site script for improved configuration clarity

--- resources/views/Scripts/SitesScripts/install-site.blade.php
+++ resources/views/Scripts/SitesScripts/install-site.blade.php
@@ -22,23 +22,23 @@ local db_vars=""
     @php
         $db_vars = "
         DB_CONNECTION=sqlite
-        #DB_HOST=$DB_HOST
-        #DB_PORT=$DB_PORT
-        #DB_DATABASE=$DB_DATABASE
-        #DB_USERNAME=$DB_USERNAME
-        #DB_PASSWORD=\"$DB_PASSWORD\"
+        #DB_HOST=127.0.0.1
+        #DB_PORT=3306
+        #DB_DATABASE=laraship
+        #DB_USERNAME=root
+        #DB_PASSWORD=
         ";
     @endphp
 @else
     @php
-        $db_vars = "
+        $db_vars = '
         DB_CONNECTION=mysql
-        DB_HOST=$DB_HOST
-        DB_PORT=$DB_PORT
-        DB_DATABASE=$DB_DATABASE
-        DB_USERNAME=$DB_USERNAME
-        DB_PASSWORD=\"$DB_PASSWORD\"
-        ";
+        DB_HOST="$DB_HOST"
+        DB_PORT="$DB_PORT"
+        DB_DATABASE="$DB_DATABASE"
+        DB_USERNAME="$DB_USERNAME"
+        DB_PASSWORD="$DB_PASSWORD"
+        ';
     @endphp
 @endif
 
