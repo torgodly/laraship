aad15d4c7fa2c6e985a363a5aaa315f26a42809f
Abdullah Alhaj <torgodly@gmail.com>
2025-03-19 00:14:35 +0200

    Debug output for PHP ini file update action in Php.php

--- app/Actions/PhpActions/UpdatePhpIniFileAction.php
+++ app/Actions/PhpActions/UpdatePhpIniFileAction.php
@@ -6,6 +6,7 @@ class UpdatePhpIniFileAction
 {
     public function execute(string $config_path, string $content): string
     {
+        dd($config_path, $content);
         $command = "echo '$content' | sudo tee -a $config_path > /dev/null";
         return shell_exec($command);
     }
