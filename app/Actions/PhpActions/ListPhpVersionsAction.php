a13025679f8fce15aa1ee317b5022be173f3bec6
Abdullah Alhaj <torgodly@gmail.com>
2025-03-19 02:17:09 +0200

    Refactor ListPhpVersionsAction to retrieve PHP versions from binaries and validate executables

--- app/Actions/PhpActions/ListPhpVersionsAction.php
+++ app/Actions/PhpActions/ListPhpVersionsAction.php
@@ -6,9 +6,21 @@ class ListPhpVersionsAction
 {
     public function execute(): array
     {
-        $output = trim(shell_exec('ls /etc/php'));
-        $versions = explode("\n", $output);
-        $versions = array_map(fn($version) => 'php' . $version, $versions);
-        return array_filter($versions);
+        $versions = [];
+        $output = shell_exec('ls /usr/bin/php*');  // List PHP binaries
+        $binaries = explode("\n", trim($output));
+
+        foreach ($binaries as $binary) {
+            if (preg_match('/php(\d+\.\d+)(?:\.\d+)?$/', basename($binary), $matches)) {
+                // Check if binary is valid PHP executable
+                $version = $matches[1];
+                if (shell_exec("php$version -v")) {
+                    $versions[] = "php$version";
+                }
+            }
+        }
+
+        return array_unique($versions);
     }
+
 }
